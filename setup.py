00fe56b72e5f0d93b55b97662d9c063ae604225b
Dustan Bower <dustan.bower@gmail.com>
2011-02-15 15:45:49 -0500

    Added text files and setup.py to repository.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup(name='pbd',
+      version='0.1.0',
+      description='Scripts for daemonizing pianobar.',
+      author='Dustan Bower',
+      author_email='dustan.bower@gmail.com',
+      scripts=['pbd', 'pbevent', 'pb.py'],
+      data_files=[('share/pbd', ['BUGS', 'COPYING', 'README', 'TODO'])])
