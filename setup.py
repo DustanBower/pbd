cc0f22d837e6565525c11a3e47337dd04b354154
Dustan Bower <dustan.bower@gmail.com>
2011-03-21 20:51:41 -0400

    setup: Version bump.  Released 0.1.1.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pbd',
-      version='0.1.0',
+      version='0.1.1',
       description='Scripts for daemonizing pianobar.',
       author='Dustan Bower',
       author_email='dustan.bower@gmail.com',
