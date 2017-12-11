89465decb628929405e206e0dead35f2e2a87b04
Dustan Bower <dustan.bower@gmail.com>
2017-12-11 14:40:28 -0500

    documentation and some slight refactoring

--- pb.py
+++ pb.py
@@ -38,6 +38,17 @@ INPUTS = (
 
 
 def display(text, heading="", timeout=DEFAULT_TIMEOUT):
+    """
+    display information to users via notifications
+
+    :param text: text to display
+    :param heading: additional information to show the user (e.g., station)
+    :param timeout: default period notification should be visible for
+    :return: None
+
+    """
+
+    # word wrap if necessary by splitting into multiple lines
     max_line_length = int(os.environ.get('PBD_MAX_LINE_LEN', 0))
 
     if max_line_length:
@@ -45,10 +56,11 @@ def display(text, heading="", timeout=DEFAULT_TIMEOUT):
     else:
         lines = [text]
 
+    # display however many notifications we have left to show
     for line in lines:
         string = "notify-send "
         if timeout:
-            string += "-t %d " % timeout 
+            string += "-t %d " % timeout
         string += "\"pbd: %s\" " % heading
         # Against all reason, & refuses to print, quoted or otherwise.
         # replacing it for now.
@@ -57,6 +69,19 @@ def display(text, heading="", timeout=DEFAULT_TIMEOUT):
 
 
 def prompt(known_inputs, display_prompt="", delay=0.5):
+    """
+    prompt user for input by loading dmenu
+
+    :param known_inputs: input choices we allow
+           ["0) Example Station", "2) Second Station"]
+    :param display_prompt: any additional information to pass
+    :param delay: how long to wait before showing prompt (default 0.5 seconds)
+           a delay that is too low will react poorly with hotkeys
+
+    :return: user selection
+
+    """
+
     time.sleep(delay)
     (BLACK, GREEN) = ("#000000", "#00EE00")
     selection = dmenu.show(known_inputs, bottom=True,
@@ -65,3 +90,16 @@ def prompt(known_inputs, display_prompt="", delay=0.5):
                            background_selected=BLACK,
                            foreground_selected=GREEN)
     return selection
+
+
+def datadict(data):
+    """
+    convert data to dictionary
+
+    :param data: data from pianobar as \n separated strings
+                    (e.g., "blah=1\nblah=2")
+    :return: dictionary using each variable as a key for the data
+                    (e.g., {'blah': 1, 'blah': 2})
+
+    """
+    return dict([line.split('=', 1) for line in data.split('\n') if line])
